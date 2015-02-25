6a0eaf64f99751f666e89cd34103a6c957733534
Alex Zai <azai91@gmail.com>
2015-02-24 18:24:28 -0800

    (feat) add mail controller

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-var mongoose = require('./server/stat/statModel');
-var
+var mongoose = require('mongoose');
+var config = require('config');
+var Stat = require('./server/stat/statModel');
+var time_yesterday = Date.now()/1000 - 24 * 60 60;
+var mailController = require('./server/mail/mailController');
+
+Stat.count({data: { $gte : time_yesterday}}, function(err, count) {
+  if (!err) {
+    mailController.sendMail(count);
+  }
+});
+
